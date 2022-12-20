f7089ccb9601d28ec8487485409f136316ce20a5
carloskauan <code.line.dark@gmail.com>
2022-12-20 01:37:13 -0300

    Adição de GET user e GET user/email

--- api/controllers/usersController.go
+++ api/controllers/usersController.go
@@ -7,8 +7,9 @@ import(
   "github.com/gin-gonic/gin"
 )
 
-func Register(c *gin.Context){
-  var user models.User
+var user models.User
+
+func RegisterUser(c *gin.Context){
   if err := c.ShouldBindJSON(&user); err != nil{
     c.JSON(http.StatusBadRequest, gin.H{
       "error": err.Error(),
@@ -17,4 +18,16 @@ func Register(c *gin.Context){
   }
   database.DB.Create(&user)
   c.JSON(http.StatusOK, user)
+}
+
+func SeachUserByEmail(c *gin.Context){
+  email := c.Params.ByName("email")
+  database.DB.Where(&models.User{Email: email}).First(&user)
+  c.JSON(http.StatusOK, user)
+}
+
+func GetAllUsers(c *gin.Context){
+  var users []models.User
+  database.DB.Find(&users)
+  c.JSON(http.StatusOK, users)
 }
