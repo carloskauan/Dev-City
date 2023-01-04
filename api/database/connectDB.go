12e6bf4cf4a802631730305e81ce3c9ae24a6141
carloskauan <code.line.dark@gmail.com>
2023-01-03 22:31:30 -0300

    Adição de autorização de cors

--- api/database/connectDB.go
+++ api/database/connectDB.go
@@ -1,23 +1,24 @@
 package database
 
-import(
-  "log"
-  "DevCity/api/models"
-  "gorm.io/gorm"
-  "gorm.io/driver/mysql"
+import (
+	"DevCity/api/models"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var (
-  DB *gorm.DB
-  err error
+	DB  *gorm.DB
+	err error
 )
 
-func ConnectDB(){
-  prepare := "root:1597@tcp(127.0.0.1:3306)/devcity?charset=utf8mb4&parseTime=True&loc=Local"
-  DB, err = gorm.Open(mysql.Open(prepare))
-  if err != nil{
-    log.Panic("Erro ao se connectar com o banco ",err)
-  }
-  DB.AutoMigrate(&models.User{})
-  DB.AutoMigrate(&models.Codes{})
+func ConnectDB() {
+	prepare := "root:15971597Ck_.@tcp(127.0.0.1:3306)/devcity?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(prepare))
+	if err != nil {
+		log.Panic("Erro ao se connectar com o banco ", err)
+	}
+	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.Codes{})
 }
